6dea0269d6b36def101dc5ee7479f49fe6634975
John <ctsoi1@ucsc.edu>
2025-10-27 16:31:11 -0700

    Update frontend structure

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,20 @@ app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
+//Search Endpoint
+app.get("/search", (req, res) => {
+  res.send("this is submit!");
+});
+
+//Event Details Endpoint
+app.get("/event/:id", (req, res) => {
+  const eventId = req.params.id;
+  res.send(`Event details for ID: ${eventId}`);
+});
+
+//Favorite Events Endpoint
+app.get("/favorites", (req, res) => {
+  res.send("List of favorite events");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
